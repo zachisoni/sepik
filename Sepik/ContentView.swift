792ef368720efc33fd6f0c14a8d93db50ff4f58b
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2025-06-12 07:57:38 +0700

    Initial Commit

---
+++ Sepik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sepik
+//
+//  Created by Asad on 12/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sepik/SepikApp.swift
@@ -0,0 +1,17 @@
+//
+//  SepikApp.swift
+//  Sepik
+//
+//  Created by Asad on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SepikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
